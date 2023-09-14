2ea0c62ec17653e704c0a233e1803feda719c65f
Nicolas Perozzi <perozzi.nicolas@gmail.com>
2023-09-14 21:57:39 +0200

    cosmetic changes

--- lib/CustomMethods/CustomMethods.cpp
+++ lib/CustomMethods/CustomMethods.cpp
@@ -10,10 +10,10 @@ Description: Compilation of methods for HalloweenPumpkin
 #include "CustomMethods.h"
 
 //Variable Definitions and Initialization
-float vol = 0.1;
+float vol = 0.02;
 CRGB Strip1[NUM_LED1];
 CRGB Strip2[NUM_LED2];
-int fieldIndex= 0;
+int fieldIndex = 0;
 uint8_t values[NUM_FIELDS];
 uint8_t serialData[NUM_FIELDS];
 
@@ -26,15 +26,17 @@ AudioConnection          patchCord1(playSdWav1, 0, amp1, 0);
 AudioConnection          patchCord2(amp1, dac1);
 // GUItool: end automatically generated code
 
-char* PlayList[] = {"Boo.wav",
-                    "Scream.wav",
-                    "EvilLaugh.wav",
-                    "Witch.wav",
-                    "Gotcha.wav",
-                    "AGoodLook.wav",
-                    "Thriler01.wav",
-                    "MountainKing.wav",
-                    "HarryPotter.wav"};
+#define num_tracks 9
+
+char PlayList[num_tracks][20] = {"Boo.wav",
+                                "Scream.wav",
+                                "EvilLaugh.wav",
+                                "Witch.wav",
+                                "Gotcha.wav",
+                                "AGoodLook.wav",
+                                "Thriler01.wav",
+                                "MountainKing.wav",
+                                "HarryPotter.wav"};
 
 
 
@@ -42,54 +44,54 @@ char* PlayList[] = {"Boo.wav",
 FlashSequence flash= FlashSequence();
 MountainKingSequence mountainKing= MountainKingSequence();
 
-void getSerialData(){
- 
-  while(Serial1.available()){
-    char ch= Serial1.read();
-    if(ch>='0' && ch<='9'){             //Add up the chars camming into the serial to build each element of the array
-      values[fieldIndex]=(values[fieldIndex]*10)+(ch-'0');
+void getSerialData()  //TODO: Why the serial data is a array of 4?
+{
+  while(Serial1.available())
+  {
+    char ch = Serial1.read();
+    if(ch >= '0' && ch <= '9')             //Add up the chars camming into the serial to build each element of the array
+    {
+      values[fieldIndex] = (values[fieldIndex] * 10) + (ch - '0');
     }
-    else if (ch==','){                  //If there is a "," it means that one emenet has ended and it is time to sart adding up the next elment
-      if(fieldIndex < NUM_FIELDS-1){
+    else if(ch == ',')                  //If there is a "," it means that one emenet has ended and it is time to sart adding up the next elment
+    {
+      if(fieldIndex < NUM_FIELDS - 1){
         fieldIndex++;
       }
     }
-    else if (ch=='F'){                  //If there is an "F", that means that one array has ended and it is time to start reading the next one.
-      for(int i=0; i<=NUM_FIELDS-1; i++){
-        serialData[i]=values[i];
-        values[i]=0;
+    else if(ch == 'F')                   //If there is an "F", that means that one array has ended and it is time to start reading the next one.
+    {
+      for(int i = 0; i <= NUM_FIELDS - 1; i++)
+      {
+        serialData[i] = values[i];
+        values[i] = 0;
       }
-      fieldIndex=0;
-/* 
-      //---ForDebuging:
-      for(int i=0; i<=NUM_FIELDS-1; i++){
-        Serial.print(serialData[i]);Serial.print(',');
-      }
-      Serial.println();
-      //--------------
-*/          
-      break;
+      fieldIndex = 0;
     }
   }
 }
 
-//Function to make the serialData arrya (0,0,0,0)
-void cleanSerialData(){
-  for(int i=0; i<=NUM_FIELDS-1; i++){   //zero serialData array for the next data.
-    serialData[i]=0; 
+//Function to make the serialData array (0,0,0,0)
+void cleanSerialData()
+{
+  for(int i = 0; i <= NUM_FIELDS - 1; i++)
+  {
+    serialData[i] = 0; 
   }
 }
 
-void Play(int value){
-  if(playSdWav1.isPlaying()==false){
-    switch(value){
-      
-      //Play: Boo!
-      case 1:
+void Play(int value)
+{
+  if(playSdWav1.isPlaying() == false)
+  {
+    switch(value)
+    {  
+      case 1: //Play: Boo!       TODO: Reduce repetition. make each case a signle function.
         Serial1.println("PlayingBoo");
-        playSdWav1.play(PlayList[value-1]);           //Playing "value-1" beause that is the position of the right song on the PlayList array.
+        playSdWav1.play(PlayList[value - 1]);           //Playing "value-1" beause that is the position of the right song on the PlayList array.
         delay(20);                                    // wait for library to pass WAV info
-        while(playSdWav1.isPlaying()==true){
+        while(playSdWav1.isPlaying() == true)
+        {
           flash.runFlashSequence(CRGB::White, 60);    //60 is the frequency of the fliquering, which seems right for the song.
           fill_solid(Strip2, NUM_LED2, CRGB::Black); //Turn off Strip2
         }
@@ -98,9 +100,10 @@ void Play(int value){
       //Play: Scream
       case 2:
         Serial1.println("PlayingScream");
-        playSdWav1.play(PlayList[value-1]);
+        playSdWav1.play(PlayList[value - 1]);
         delay(20);
-        while(playSdWav1.isPlaying()==true){
+        while(playSdWav1.isPlaying() == true)
+        {
           flash.runFlashSequence(CRGB::Green, 60); 
           fill_solid(Strip2, NUM_LED2, CRGB::Black);
         }
@@ -109,9 +112,10 @@ void Play(int value){
       //Play: EvilLaugh
       case 3:
         Serial1.println("PlayingEvilLaugh");
-        playSdWav1.play(PlayList[value-1]);
+        playSdWav1.play(PlayList[value - 1]);
         delay(20);
-        while(playSdWav1.isPlaying()==true){
+        while(playSdWav1.isPlaying() == true)
+        {
           flash.runFlashSequence(CRGB::Red, 60);
           fill_solid(Strip2, NUM_LED2, CRGB::Black);
         }
@@ -120,9 +124,10 @@ void Play(int value){
       //Play: Witch
       case 4:
         Serial1.println("PlayingWitch");
-        playSdWav1.play(PlayList[value-1]);
+        playSdWav1.play(PlayList[value - 1]);
         delay(20);
-        while(playSdWav1.isPlaying()==true){
+        while(playSdWav1.isPlaying() == true)
+        {
           flash.runFlashSequence(CRGB::Green, 60);
           fill_solid(Strip2, NUM_LED2, CRGB::Black);
         }
@@ -131,9 +136,10 @@ void Play(int value){
       //Play: Gotcha
       case 5:
         Serial1.println("PlayingGotcha");
-        playSdWav1.play(PlayList[value-1]);
+        playSdWav1.play(PlayList[value - 1]);
         delay(20);
-        while(playSdWav1.isPlaying()==true){
+        while(playSdWav1.isPlaying() == true)
+        {
           flash.runFlashSequence(CRGB::Red, 60);
           fill_solid(Strip2, NUM_LED2, CRGB::Black); 
         }
@@ -142,9 +148,10 @@ void Play(int value){
       //Play: AGoodLook
       case 6:
         Serial1.println("PlayingAGoodLook");
-        playSdWav1.play(PlayList[value-1]);
+        playSdWav1.play(PlayList[value - 1]);
         delay(20);
-        while(playSdWav1.isPlaying()==true){
+        while(playSdWav1.isPlaying() == true)
+        {
           flash.runFlashSequence(CRGB::White, 60); 
           fill_solid(Strip2, NUM_LED2, CRGB::Black);
         }
@@ -153,9 +160,9 @@ void Play(int value){
       //Play: Thriler01
       case 7:
         Serial1.println("PlayingThriler");
-        playSdWav1.play(PlayList[value-1]);
+        playSdWav1.play(PlayList[value - 1]);
         delay(20);
-        while(playSdWav1.isPlaying()==true){
+        while(playSdWav1.isPlaying() == true) {
           FlashMultiColor();
         }
         break;
@@ -163,14 +170,16 @@ void Play(int value){
       //Play: MountainKing
       case 8:
         Serial1.println("PlayingMountainKing");
-        playSdWav1.play(PlayList[value-1]);
+        playSdWav1.play(PlayList[value - 1]);
         delay(20);
-        while(playSdWav1.isPlaying()==true){
+        while(playSdWav1.isPlaying() == true)
+        {
           mountainKing.runMountainKingSequence(500);
           
           //MountainKing boo flash
           unsigned int time1=6858;
-          while(playSdWav1.positionMillis()>=time1){
+          while(playSdWav1.positionMillis() >= time1)
+          {
             flash.runFlashSequence(CRGB::White, 60);
             fill_solid(Strip2, NUM_LED2, CRGB::White);
           }
@@ -180,10 +189,11 @@ void Play(int value){
       //Play: HarryPotter
       case 9:
         Serial1.println("PlayingHarryPotter");
-        playSdWav1.play(PlayList[value-1]);
+        playSdWav1.play(PlayList[value - 1]);
         delay(20);
-        while(playSdWav1.isPlaying()==true){
-            mountainKing.runMountainKingSequence(1000);
+        while(playSdWav1.isPlaying() == true)
+        {
+          mountainKing.runMountainKingSequence(1000);
         }
         break;
     }
@@ -191,14 +201,17 @@ void Play(int value){
 }
 
 void Candles(){
-  if(playSdWav1.isPlaying()==false){
-    for(int i = 0; i < NUM_LED1; i++) {
+  if(playSdWav1.isPlaying() == false)
+  {
+    for(int i = 0; i < NUM_LED1; i++)
+    {
       float f = sin(millis() * 0.002f + (i * 0.628));
       int c = max(0, (int)(f * f * f * 50));
       Strip1[i]= CRGB(makeColor(10, 100, c));
       FastLED.show(); 
     }
-    for(int i = 0; i < NUM_LED2; i++) {
+    for(int i = 0; i < NUM_LED2; i++)
+    {
       float f = sin(millis() * 0.002f + (i * 0.628));
       int c = max(0, (int)(f * f * f * 50));
       Strip2[i]= CRGB(makeColor(10, 100, c));
@@ -207,21 +220,25 @@ void Candles(){
   }
 }
 
-void FlashMultiColor(){
+void FlashMultiColor()
+{
   #define RED    0xFF0000
   #define GREEN  0x00FF00
   #define BLUE   0x0000FF
-  int numColors=3;
-  int colors[]={RED,BLUE,GREEN};
-  int tempo=250;
-  while(playSdWav1.isPlaying()==true){
-    for(int j=0; j<numColors;j++){
-      for (int i=0; i < NUM_LED1; i++) {
-        Strip1[i]=colors[j];
+  int numColors = 3;
+  int colors[] = {RED, BLUE, GREEN};
+  int tempo = 250;
+  while(playSdWav1.isPlaying() == true)
+  {
+    for(int j = 0; j<numColors;j++)
+    {
+      for(int i = 0; i < NUM_LED1; i++)
+      {
+        Strip1[i] = colors[j];
         FastLED.show(); 
       }
-      for (int i=0; i < NUM_LED2; i++) {
-        Strip2[i]=colors[j];
+      for (int i = 0; i < NUM_LED2; i++) {
+        Strip2[i] = colors[j];
         FastLED.show(); 
       }
       delay(tempo);
@@ -229,53 +246,60 @@ void FlashMultiColor(){
   }
 }
 
-void Volume(int value){
-  switch (value){
+void Volume(int value)
+{
+  switch (value)  //TODO: Maybe this can be an if instead of a switch?
+  {
     case 101:
-      vol=vol+0.1;
-      if(vol>=0.6){
+      vol = vol + 0.02; //TODO: No hardcoded values
+      if(vol >= 0.6)   //TODO: Can I use a "constrain" function here?
+      {
         vol=0.6;
       }
       amp1.gain(vol);
       Serial1.println(vol);
-      Serial.println(vol);
+      Serial.println(vol);  //DEBUGGING LINE
       break;
       
     case 100:
-      vol=vol-0.1;
-      if(vol>=0.6){         //TODO: Test if you can push the limit higher
+      vol=vol-0.02;
+      if(vol>=0.6)           //TODO: Test if you can push the limit higher
+      {
         vol=0.6;
       }
       amp1.gain(vol);
       Serial1.println(vol);
-      Serial.println(vol);
+      Serial.println(vol); //DEBUGGING LINE
       break;
   }
 }
 
-void MountainKingSequence::runMountainKingSequence(int eventFrequency){
-    fill_solid(Strip1, NUM_LED1, CRGB::Black);
-    
-    //MiniPumpkins Dance
-    EVERY_N_MILLISECONDS(eventFrequency){
-      //Create a new pixel for led[0]:
-      Strip2[0]=CHSV(random8(), random8(), random8(100, 255));
-    
-      //Copy each pixel to the next one
-      for( int i=3-1; i>0; i--){
-        Strip2[i]= Strip2[i-1];
-      }
-      FastLED.show();
+void MountainKingSequence::runMountainKingSequence(int eventFrequency)
+{
+  fill_solid(Strip1, NUM_LED1, CRGB::Black);
+  //MiniPumpkins Dance
+  EVERY_N_MILLISECONDS(eventFrequency)
+  {
+    //Create a new pixel for led[0]:
+    Strip2[0]=CHSV(random8(), random8(), random8(100, 255));
+  
+    //Copy each pixel to the next one
+    for( int i=3-1; i>0; i--)
+    {
+      Strip2[i]= Strip2[i-1];
     }
+    FastLED.show();
+  }
 }
 
-void FlashSequence::runFlashSequence(CRGB color, uint32_t eventFrequency){
-  uint32_t timeStamp= millis();
-
-  if((timeStamp - lastEvent)>= eventFrequency){
+void FlashSequence::runFlashSequence(CRGB color, uint32_t eventFrequency)
+{
+  uint32_t timeStamp = millis();
+  if((timeStamp - lastEvent) >= eventFrequency)
+  {
     fill_solid(Strip1, NUM_LED1, isOn? CRGB::Black : color);
-    isOn=!isOn;
-    lastEvent=millis();
+    isOn =! isOn;
+    lastEvent = millis();
   }
   FastLED.show();  
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,25 @@
-/* HalloweenPumpkinV2.1Code-CURRENT
-What is changing:
-- Adding the ColorWheel functionality
-
+/* HalloweenPumpkinV3
 Decription:
   A HalloweenPumpkin that reproduces "tracks" (sound and a light sequence) when triggered via bluetooth.
   The bluetooth transmitor the MIT App Inventory app "HalloweenPumpkinAppV2"
 
 Hardware:
   This code has been made to work on a Teensy 3.5 according to the schematics in the project files.
-
-VersionTracker:
-V1.0: 
-  - Sequences with audio and flashing LEDs
-  - IR sensor
-  - PIR sensor
-V2.0:
-  - MountainKing sequence has been added but not fully developed.
-  - LED flash sequence has been added.
-  - PIR sensor functionality has been removed.
-  - Bluetooth has been added instead of IR sensor
-  - App on MIT Inventor
-  - Harry Potter Track
-V2.1:
-  - ColorWheel functionality
-  - Changed the app to "HalloweenPumpkinAppV2"
+Version:
+  Code migrated to PlatformIO
 */ 
-//Audio Libraries
-//#include <Audio.h>
-//#include <SerialFlash.h>
-//#include <SPI.h>
-//#include <SD.h>
-//#include <FastLED.h>
 #include "CustomMethods.h"
 
 //Desabling Interrupts
-  #define FASTLED_ALLOW_INTERRUPTS 0  //TODO: Do I need to desable interrupts?
+//#define FASTLED_ALLOW_INTERRUPTS 0  //NOTE: seems to work without desabling interrupts.
 
 //SD Card elements:
-  #define BUILTIN_SDCARD
-  #define SDCARD_MOSI_PIN 11
-  #define SDCARD_SCK_PIN 13
+#define BUILTIN_SD_CARD BUILTIN_SDCARD
+#define SDCARD_MOSI_PIN 11
+#define SDCARD_SCK_PIN 13
 
-void setup() {
+void setup()
+{
   //Bluetooth Serial setup
   Serial1.begin(9600);  //Defining communication rate with the HC-06 bluetooth module. Boud rate is 9600
   Serial.begin(9600);   //Serial for debugging
@@ -53,30 +31,27 @@ void setup() {
   SPI.setMOSI(SDCARD_MOSI_PIN);
   SPI.setSCK(SDCARD_SCK_PIN);
 
-  if (!SD.begin(BUILTIN_SDCARD)) {
+  if (!SD.begin(BUILTIN_SD_CARD))
+  {
     Serial1.println("Unable to access the SD card");
     delay(500);
   }
   
   //FastLED Setup:
-  //Candles Strip setup:
-  FastLED.addLeds<WS2812, LED1_PIN, RGB> (Strip1, NUM_LED1);
-  //MiniPumpkins Strip setup:
-  FastLED.addLeds<WS2812, LED2_PIN, RGB> (Strip2, NUM_LED2);
+  FastLED.addLeds<WS2812, LED1_PIN, RGB> (Strip1, NUM_LED1); //Candles Strip setup
+  FastLED.addLeds<WS2812, LED2_PIN, RGB> (Strip2, NUM_LED2); //MiniPumpkins Strip setup
   FastLED.setBrightness(50);
 
   //Volume Setup:
-    amp1.gain(vol);                      //vol is a float. vol=0 is not sound. 0<vol<1.0 atenuation. 1.0<vol<32767.0
-    Serial1.println(vol);
-
-  delay(500);     //TODO: Do I need this delay? why?
+  amp1.gain(vol);   //vol is a float. vol=0 is not sound. 0.0<vol<1.0 atenuation. 1.0<vol<32767.0
+  Serial1.println(vol);
 }
 
-void loop() {
-  getSerialData();                      //Function to read the serial data coming from the app via bluetooth
-  
-  //Select track
-  switch(serialData[0]){
+void loop()
+{
+  getSerialData();
+  switch(serialData[0])
+  {
     //Track: Candles. This is the default track since every time a track finished the serialData array is reset to (0,0,0,0)
     case 0:
       Candles();
